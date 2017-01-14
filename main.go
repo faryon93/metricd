15ecf451ffdb3d33bd194d33b260d4b315abc775
Maximilian Pachl <m@ximilian.info>
2017-01-14 13:06:34 +0100

    moved snmp watcher to extra package

--- main.go
+++ main.go
@@ -29,19 +29,15 @@ import (
     "os/signal"
 
     "github.com/faryon93/metricd/config"
+    "github.com/faryon93/metricd/snmp"
 
     "github.com/influxdata/influxdb/client/v2"
-    "github.com/alouca/gosnmp"
 )
 
 
 // --------------------------------------------------------------------------------------
 //  constants
 // --------------------------------------------------------------------------------------
-
-const (
-    INFLUX_PRECISION = "ms"
-)
 
 
 // --------------------------------------------------------------------------------------
@@ -83,9 +79,9 @@ func main() {
     log.Println(influx.Ping(300 * time.Millisecond))
 
     // setup all measurement watcher
-    for name, snmp := range conf.Snmp {
+    for name, snmpConf := range conf.Snmp {
         log.Printf("setting up snmp target \"%s\"", name)
-        go watcher(influx, snmp)
+        go snmp.Watcher(influx, snmpConf)
     }
 
     // wait for signals to exit application
@@ -95,58 +91,6 @@ func main() {
 // --------------------------------------------------------------------------------------
 //  helper functions
 // --------------------------------------------------------------------------------------
-
-func watcher(influxdb client.Client, snmpConf config.SnmpConf) {
-    for {
-        // esthablish snmp connection
-        snmp, err := gosnmp.NewGoSNMP(snmpConf.Host, snmpConf.Community, gosnmp.Version2c, 1)
-        if err != nil {
-            log.Fatal(err)
-        }
-
-        // gather all datapoints
-        values := make(map[string]interface{})
-        for _, oid := range snmpConf.DataPoints {
-            // query the snmp oid for its values
-            resp, err := snmp.Get(oid.Key())
-            if err != nil {
-               log.Println("failed to fetch snmp oid:", err.Error())
-                continue
-            }
-
-            // make sure the response is not empty
-            if len(resp.Variables) <= 0 {
-                log.Println("empty response for oid", oid.Key())
-                continue
-            }
-
-            values[oid.Val()] = int(resp.Variables[0].Value.(uint64))
-        }
-
-        bp, _ := client.NewBatchPoints(client.BatchPointsConfig{
-            Database:  snmpConf.Database,
-            Precision: INFLUX_PRECISION,
-        })
-
-        // construct the new datapoint
-        pt, _ := client.NewPoint(
-            snmpConf.Measurement,
-            config.GetPairSlice(snmpConf.Tags).Map(),
-            values,
-            time.Now(),
-        )
-        bp.AddPoint(pt)
-
-        // write the datapoints
-        err = influxdb.Write(bp)
-        if err != nil {
-            log.Println("failed to write datapoint:", err.Error())
-        }
-
-        // sleep until next execution
-        time.Sleep(time.Duration(snmpConf.SampleTime) * time.Millisecond)
-    }
-}
 
 func wait(sig ...os.Signal) {
     signals := make(chan os.Signal)
